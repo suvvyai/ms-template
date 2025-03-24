d23f9dc82a0660bd2b837d8f863fa7138a0c0309
sushka <r.m.poltorabatko@gmail.com>
2025-03-24 15:29:44 +0300

    add tests

---
+++ tests/test_app.py
@@ -0,0 +1,23 @@
+import random
+import string
+from collections.abc import AsyncGenerator
+
+import pytest
+from httpx import ASGITransport, AsyncClient
+
+from src.app.app import app
+
+
+@pytest.fixture(scope="session")
+async def client() -> AsyncGenerator[AsyncClient]:
+    """Получить тестовый клиент"""
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://testserver") as client:
+        yield client
+
+
+@pytest.mark.parametrize("name", ["".join(random.choices(string.ascii_letters, k=10)) for _ in range(10)])
+async def test_hello_world(client: AsyncClient, name: str) -> None:
+    response = await client.post("/api/hi", json={"name": name})
+
+    assert response.status_code == 200
+    assert response.json() == {"greeting": f"Hello, {name}!"}
