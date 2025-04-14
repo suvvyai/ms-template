57e074c7280bbcd8ce0c0a1c347f62880beca7c6
sushka <r.m.poltorabatko@gmail.com>
2025-04-14 17:19:05 +0300

    add initialize_database

---
+++ src/services/database.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+from beanie import init_beanie
+from loguru import logger
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from services import settings
+
+
+async def initialize_database() -> None:
+    logger.info("Initialising DB...")
+    kwargs: dict[str, Any] = {}
+    if settings.mongo.cert_file_path:
+        kwargs |= {"tlsCAFile": settings.mongo.cert_file_path}
+
+    await init_beanie(
+        database=AsyncIOMotorClient(settings.mongo.url, **kwargs).get_database(settings.mongo.db_name),
+        document_models=[],
+    )
+    logger.success("DB is ready!")
