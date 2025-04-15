f7aa833a518796deacf0e16fb7a570ebbde2c717
sushka <r.m.poltorabatko@gmail.com>
2025-04-15 19:42:00 +0300

    split files

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,23 +1,8 @@
 import random
 import string
-from collections.abc import AsyncGenerator
 
 import pytest
-from httpx import ASGITransport, AsyncClient
-
-from core import settings
-from src.app.app import app
-
-
-@pytest.fixture(scope="session")
-async def client() -> AsyncGenerator[AsyncClient]:
-    """Получить тестовый клиент"""
-    async with AsyncClient(
-        transport=ASGITransport(app=app),
-        base_url="http://testserver",
-        headers={"Microservice-Api-Key": settings.microservices.key},
-    ) as client:
-        yield client
+from httpx import AsyncClient
 
 
 @pytest.mark.parametrize("name", ["".join(random.choices(string.ascii_letters, k=10)) for _ in range(10)])
