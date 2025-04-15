fea67f40ad2a972d896b403f91015e69d5302405
sushka <r.m.poltorabatko@gmail.com>
2025-04-15 19:41:35 +0300

    add headers

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,13 +5,18 @@ from collections.abc import AsyncGenerator
 import pytest
 from httpx import ASGITransport, AsyncClient
 
+from core import settings
 from src.app.app import app
 
 
 @pytest.fixture(scope="session")
 async def client() -> AsyncGenerator[AsyncClient]:
     """Получить тестовый клиент"""
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://testserver") as client:
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url="http://testserver",
+        headers={"Microservice-Api-Key": settings.microservices.key},
+    ) as client:
         yield client
 
 
