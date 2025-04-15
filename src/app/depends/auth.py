a8cca6b82a4e4638879249fe5ef39dc1e04c51e9
sushka <r.m.poltorabatko@gmail.com>
2025-04-15 19:02:35 +0300

    add microservice_auth

--- src/app/depends/auth.py
+++ src/app/depends/auth.py
@@ -1,22 +1,17 @@
+from typing import Annotated
+
 from fastapi import Depends
-from fastapi.openapi.models import APIKey, APIKeyIn, SecurityBase
-from fastapi.requests import Request
+from fastapi.security import APIKeyHeader
 
 from core import settings
 from core.errors import SuvvyError
 
 
-class MicroserviceAuthentication(SecurityBase):
-    model: APIKey = APIKey(name="Microservice-Api-Key", **{"in": APIKeyIn.header})  # type: ignore[arg-type]
-
-    def __init__(self, api_key: str, scheme_name: str | None = None):
-        self.api_key = api_key
-        self.scheme_name = scheme_name or self.__class__.__name__
-
-    def __call__(self, request: Request) -> None:
-        header_api_key: str | None = request.headers.get("Microservice-Api-Key", None)
-        if header_api_key is None or header_api_key != self.api_key:
-            raise SuvvyError.AUTH_INVALID_TOKEN.as_exception()
+async def microservice_auth(
+    api_key: Annotated[str, Depends(APIKeyHeader(name="Microservice-Api-Key"))],
+) -> None:
+    if api_key != settings.microservices.key:
+        raise SuvvyError.AUTH_INVALID_TOKEN.as_exception()
 
 
-MicroserviceAuth = Depends(MicroserviceAuthentication(api_key=settings.microservices.key))
+MicroserviceAuth = Depends(microservice_auth)
