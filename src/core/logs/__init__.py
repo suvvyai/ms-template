72e47691b07fab54dcabd13afb2a8cfb148cb4da
sushka <r.m.poltorabatko@gmail.com>
2026-08-04 20:15:00 +0300

    fix: avoid runtime loguru Record import

--- src/core/logs/__init__.py
+++ src/core/logs/__init__.py
@@ -1,11 +1,14 @@
 import sys
+from typing import TYPE_CHECKING
 
 from loguru import logger
-from loguru._logger import Record
 
 from core.logs.handlers import UvicornHandler
 
 __all__ = ["UvicornHandler", "configure_logger", "get_uvicorn_log_config"]
+
+if TYPE_CHECKING:
+    from loguru._record import Record
 
 
 def configure_logger() -> None:
@@ -14,7 +17,7 @@ def configure_logger() -> None:
         "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | {message} {exception}\n"
     )
 
-    def log_format(record: Record) -> str:
+    def log_format(record: "Record") -> str:
         if record["level"].name == "REQUEST":
             return log_format_request
         return log_format_all
